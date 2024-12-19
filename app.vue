e08ff502bd994dd56c1fdeb92745df5a7bc51bf7
cadamsdev <cadamsdev@outlook.com>
2024-12-19 15:42:25 -0500

    feat: added sanity studio

--- app.vue
+++ app.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+
+    <h1>Movies</h1>
+    <ul>
+      <li v-for="movie in movies" :key="movie._id">
+        {{ movie.title }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+  type MovieQueryResult = {
+    title: string;
+    _id: string;
+  }
+
+  const query = `
+    *[_type == "movie"] {
+      _id,
+      title
+    }
+  `;
+
+  const { data: movies } = await useSanityQuery<MovieQueryResult[]>(query);
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/sanity'],
+  sanity: {
+    projectId: 'v39h2x7o',
+    dataset: 'production',
+    minimal: true,
+  }
 });
